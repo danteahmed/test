c34e7098fdc1dcc2601e16edd4f1a5948fb2c753
danteahmed <62128195+danteahmed@users.noreply.github.com>
2021-02-02 19:17:36 +0600

    Add files via upload

---
+++ myform.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration Page </title>
+<head>
+<body style="background-color:powderblue;">
+
+<h1>My Registration Page</h1>
+
+<form>
+<table>
+<tr><td>Name: </td>
+<td><input type="text" name="name" ></td></tr>
+<br>
+<br>
+<tr>
+<td>Email: </td>
+<td><input type="text" name="email"></td></tr>
+<br>
+<br>
+<tr>
+<td>User Name: </td>
+<td><input type="text" name="username"></td></tr>
+<br>
+<br>
+<tr>
+<td>Password: </td>
+<td><input type="password" name="password"></td></tr>
+<br>
+<br>
+<tr>
+<td>Confirm Password: </td>
+<td><input type="Password" name="cpassword"></td></tr>
+<br>
+<br>
+<tr>
+<td> Gender: </td></tr>
+<br>
+<tr><td>  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label></td>
+  <td>  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">female</label></td>
+  <td>  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">other</label></td>
+<br>
+<tr>
+<td> Date Of Birth: </td></tr>
+<tr><td><input type="date" id="birthday" name="birthday"></td>
+</tr>
+<br>
+<br>
+<tr>
+<td><input type="submit" value="Submit"></td>
+<td><input type="reset" value="Reset"></td></tr>
+
+</form> 
+
+
+</body>
+</html>
